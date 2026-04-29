ebc750391d4acd79658b2c52df385757dac54fac
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-04-29 17:07:38 +0700

    Replace JwtAuthGuard with custom guard

--- src/auth/guards/auth.guard.spec.ts
+++
@@ -1,8 +0,0 @@
-import { JwtAuthGuard } from './auth.guard';
-import { describe, it, expect } from '@jest/globals';
-
-describe('JwtAuthGuard', () => {
-  it('should be defined', () => {
-    expect(new JwtAuthGuard()).toBeDefined();
-  });
-});
--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,5 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private reflector: Reflector,
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    // const requiredRoles = this.reflector.getAllAndOverride<string[]>('roles', [
+    //   context.getHandler(),
+    //   context.getClass(),
+    // ]);
+    // console.log(
+    //   '🚀 ~ RolesGuard ~ canActivate ~ requiredRoles:',
+    //   requiredRoles,
+    // );
+
+    // if (!requiredRoles) {
+    //   return true;
+    // }
+
+    const request = context.switchToHttp().getRequest();
+
+    const authHeader = request.headers['authorization'];
+
+    if (!authHeader) {
+      throw new ForbiddenException(
+        'No identity verification information available.',
+      );
+    }
+
+    const token = request.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw new ForbiddenException('Token not found');
+    }
+
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.configService.get<string>('JWT_ACCESS_SECRET'),
+      });
+      if (!payload) {
+        throw new ForbiddenException('Token not valid');
+      }
+      request.user = payload.id;
+
+      return true;
+    } catch (error) {
+      throw new ForbiddenException(
+        'The account does not have the authority to perform this action.',
+      );
+    }
+  }
+}
