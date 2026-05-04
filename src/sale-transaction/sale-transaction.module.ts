cba1aff009970f00533df074eb0bfd1dd08c961b
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-04 16:15:03 +0700

    Wire mongoose and repository into module

--- src/sale-transaction/sale-transaction.module.ts
+++ src/sale-transaction/sale-transaction.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { SaleTransactionController } from './controllers/sale-transaction.controller';
 import { SaleTransactionService } from './services/sale-transaction.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  SalesTransaction,
+  SalesTransactionSchema,
+} from './schemas/sale-transaction.schema';
+import { LoggerService } from '@common/logs/logger.service';
+import { JwtAuthGuard } from '@auth/guards/auth.guard';
+import { SaleTransactionRepository } from './repositories/sale-transaction.repository';
 
 @Module({
-  providers: [SaleTransactionService],
+  imports: [
+    MongooseModule.forFeature([
+      { name: SalesTransaction.name, schema: SalesTransactionSchema },
+    ]),
+  ],
+  providers: [
+    SaleTransactionService,
+    SaleTransactionRepository,
+    LoggerService,
+    JwtAuthGuard,
+  ],
   controllers: [SaleTransactionController],
+  exports: [SaleTransactionService, SaleTransactionRepository, MongooseModule],
 })
 export class SaleTransactionModule {}
