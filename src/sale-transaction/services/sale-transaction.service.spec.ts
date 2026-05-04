c3573532eddd9d9ae123eb3ae8d09fabd03a1e10
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-04 14:42:57 +0700

    Add unit tests for controller and service

---
+++ src/sale-transaction/services/sale-transaction.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { describe, it, expect, beforeEach } from '@jest/globals';
+import { SaleTransactionService } from './sale-transaction.service';
+
+describe('SaleTransactionService', () => {
+  let service: SaleTransactionService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [SaleTransactionService],
+    }).compile();
+
+    service = module.get<SaleTransactionService>(SaleTransactionService);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+});
