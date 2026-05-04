9b4c17e148199e5baa382bbccbad7c77018c5a06
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-04 11:19:46 +0700

    Adjust module wiring for auth and users

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
-import { UsersModule } from '@users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from '../common/constants/jwt.const';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '@users/schemas/users.schema';
 import { JwtStrategy } from './strategies/auth.strategy';
 import { JwtAuthGuard } from './guards/auth.guard';
+import { UsersModule } from '@users/users.module';
 
 @Module({
   imports: [
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,17 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { UsersService } from '@users/services/users.service';
 import { UsersController } from '@users/controllers/users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersRepository } from './repositories/users.repository';
 import { LoggerService } from '@common/logs/logger.service';
 import { User, UserSchema } from './schemas/users.schema';
+import { JwtAuthGuard } from '@auth/guards/auth.guard';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
   ],
   controllers: [UsersController],
-  providers: [UsersService, UsersRepository, LoggerService],
+  providers: [UsersService, UsersRepository, LoggerService, JwtAuthGuard],
   exports: [
     UsersService,
     UsersRepository,
