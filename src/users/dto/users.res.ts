436f551d53c67978d231b6862b33de929fd85f22
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-04 12:17:10 +0700

    Enhances users response DTO serialization

--- src/users/dto/users.res.ts
+++ src/users/dto/users.res.ts
@@ -1,10 +1,22 @@
-import { Exclude } from 'class-transformer';
+import { Exclude, Expose, Transform } from 'class-transformer';
 
 export class UsersResponseDTO {
+  @Expose()
+  @Transform(({ obj }) => obj._id?.toString())
   _id: string;
+
+  @Expose()
   username: string;
+
   @Exclude()
   password: string;
+
+  @Expose()
+  role: string;
+
+  @Expose()
   createdAt: Date;
+
+  @Expose()
   updatedAt: Date;
 }
