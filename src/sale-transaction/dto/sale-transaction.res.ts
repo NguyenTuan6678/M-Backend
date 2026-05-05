c37ebf941351a48fd87e13b98b9fb26ab3e9ef3e
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 10:34:08 +0700

    Clean up unused imports from response DTOs

--- src/sale-transaction/dto/sale-transaction.res.ts
+++ src/sale-transaction/dto/sale-transaction.res.ts
@@ -1,5 +1,4 @@
 import { MessageResponse } from '@app-types/message.res';
-import { Expose, Transform } from 'class-transformer';
 import { SalesTransaction } from '../schemas/sale-transaction.schema';
 
 export class SaleTransactionResponseDTO extends MessageResponse {
