f45c7575095e052ff875b6fb76bbfb50038050fb
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 14:25:37 +0700

    Refactor users response DTO to use message response
    Simplifies the users response DTO by extending MessageResponse and removing explicit field decorators.

--- src/users/dto/users.res.ts
+++ src/users/dto/users.res.ts
@@ -1,22 +1,20 @@
+import { MessageResponse } from '@app-types/message.res';
+import { User } from '@users/schemas/users.schema';
 import { Exclude, Expose, Transform } from 'class-transformer';
 
-export class UsersResponseDTO {
-  @Expose()
-  @Transform(({ obj }) => obj._id?.toString())
-  _id: string;
-
-  @Expose()
-  username: string;
-
-  @Exclude()
-  password: string;
-
-  @Expose()
-  role: string;
-
-  @Expose()
-  createdAt: Date;
-
-  @Expose()
-  updatedAt: Date;
+export class UsersResponseDTO extends MessageResponse {
+  content?: User;
+  // @Expose()
+  // @Transform(({ obj }) => obj._id?.toString())
+  // _id: string;
+  // @Expose()
+  // username: string;
+  // @Exclude()
+  // password: string;
+  // @Expose()
+  // role: string;
+  // @Expose()
+  // createdAt: Date;
+  // @Expose()
+  // updatedAt: Date;
 }
