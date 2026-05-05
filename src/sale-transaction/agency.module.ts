85f9a3ebfdbb90b3e1757006eeaeefff9f2557d9
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 11:45:12 +0700

    Add agency module with controller, service, and repository

---
+++ src/sale-transaction/agency.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { Agency, AgencySchema } from './schemas/agency.schema';
+import { MongooseModule } from '@nestjs/mongoose/dist/mongoose.module';
+import { AgencyController } from './controllers/agency.controller';
+import { AgencyService } from './services/agency.service';
+import { AgencyRepository } from './repositories/agency.repository';
+import { LoggerService } from '@common/logs/logger.service';
+import { JwtAuthGuard } from '@auth/guards/auth.guard';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Agency.name, schema: AgencySchema }]),
+  ],
+  controllers: [AgencyController],
+  providers: [AgencyService, AgencyRepository, LoggerService, JwtAuthGuard],
+  exports: [AgencyService, AgencyRepository, MongooseModule],
+})
+export class AgencyModule {}
