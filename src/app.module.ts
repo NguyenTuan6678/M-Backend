5b3d4b692119a218fa93b5da0ce01c6fdfaad014
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 15:57:31 +0700

    Uses path aliases and registers AgencyModule
    Switch imports to path aliases and register the new agency module in the application module; add a config path alias to TypeScript to support the aliased configuration import.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './users/auth/auth.module';
-import configuration from './config/configuration';
+import { UsersModule } from '@users/users.module';
+import { AuthModule } from '@users/auth/auth.module';
+import configuration from '@config/configuration';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { SaleTransactionModule } from './sale-transaction/sale-transaction.module';
+import { SaleTransactionModule } from '@transaction/sale-transaction.module';
+import { AgencyModule } from '@agency/agency.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { SaleTransactionModule } from './sale-transaction/sale-transaction.modul
     }),
     UsersModule,
     SaleTransactionModule,
+    AgencyModule,
     AuthModule,
   ],
   controllers: [AppController],
