76650cf7bfe8da564560a95f24f10862c8287d97
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 16:20:22 +0700

    Adds API metadata to create agency DTO

--- src/agency/dto/create-agency.req.ts
+++ src/agency/dto/create-agency.req.ts
@@ -6,20 +6,29 @@ import {
   IsString,
 } from 'class-validator';
 import { Type } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateAgencyDto {
-  @IsString()
-  @IsNotEmpty()
-  code: string;
-
+  @ApiProperty({
+    description: 'Name of the agency',
+    example: 'Best Travel Agency',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Commission percentage for the agency',
+    example: 10,
+  })
   @Type(() => Number)
   @IsNumber()
   commissionPercent: number;
 
+  @ApiProperty({
+    description: 'Indicates if the agency is active',
+    example: true,
+  })
   @IsBoolean()
   @IsOptional()
   isActive?: boolean;
