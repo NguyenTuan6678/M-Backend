d8cbffbc50f54c6dfcffa60e8900e196839e5f99
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 22:42:23 +0700

    Adds bank controller and service logic
    Introduce endpoints and service methods to manage bank records with proper pagination and error handling, enabling creation, retrieval, update, and deletion flows for the banking domain.

--- src/bank/bank.controller.ts
+++ src/bank/bank.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { BankService } from './bank.service';
+import { ApiOperation } from '@nestjs/swagger';
+import { CreateBankDto } from './dto/create-bank.req';
+import { BankResponseDto } from './dto/bank.res';
+import {
+  PaginatedResponseDto,
+  PaginationDto,
+} from '@common/dto/pagination.dto';
+import { MessageResponse } from '@app-types/message.res';
 
 @Controller('banks')
-export class BankController {}
+export class BankController {
+  constructor(private readonly bankService: BankService) {}
+
+  @Post('create')
+  @ApiOperation({ summary: 'Create a new bank' })
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body(ValidationPipe) createBankDto: CreateBankDto,
+  ): Promise<BankResponseDto> {
+    return await this.bankService.createBank(createBankDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get a paginated list of banks' })
+  async findAll(
+    @Query(ValidationPipe) paginationDto: PaginationDto,
+  ): Promise<PaginatedResponseDto<BankResponseDto>> {
+    return await this.bankService.getAllBanks(paginationDto);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Updated bank by ID' })
+  async update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateBankDto: Partial<CreateBankDto>,
+  ): Promise<BankResponseDto> {
+    return await this.bankService.updateBank(id, updateBankDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete bank by ID' })
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string): Promise<MessageResponse> {
+    return await this.bankService.deleteBank(id);
+  }
+}
--- src/bank/bank.service.ts
+++ src/bank/bank.service.ts
@@ -1,5 +1,5 @@
 import { LoggerService } from '@common/logs/logger.service';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { BankRepository } from '@repositories/bank.repository';
 import { Bank } from '@schemas/bank.schema';
@@ -8,6 +8,10 @@ import { CreateBankDto } from './dto/create-bank.req';
 import { BankResponseDto } from './dto/bank.res';
 import { MessageResponse } from '@app-types/message.res';
 import { ERROR_RES } from '@common/constants/error.const';
+import {
+  PaginatedResponseDto,
+  PaginationDto,
+} from '@common/dto/pagination.dto';
 
 @Injectable()
 export class BankService {
@@ -62,5 +66,62 @@ export class BankService {
       };
     }
     return response;
+  }
+
+  async getBankById(id: string): Promise<BankResponseDto> {
+    const bank = await this.bankRepository.findById(id);
+    if (!bank) {
+      throw new NotFoundException(`Bank with ID ${id} dose not in database`);
+    }
+    return this.mapToResponseDto(bank);
+  }
+
+  private mapToResponseDto(user: any): BankResponseDto {
+    const response = new BankResponseDto();
+    response.content = user.toObject();
+    return response;
+  }
+
+  async getAllBanks(
+    paginationDto: PaginationDto,
+  ): Promise<PaginatedResponseDto<BankResponseDto>> {
+    const { data, total } = await this.bankRepository.findAll(
+      paginationDto.skip,
+      paginationDto.limit,
+    );
+    return {
+      data: data.map((bank) => this.mapToResponseDto(bank)),
+      total,
+      page: paginationDto.page,
+      limit: paginationDto.limit,
+      totalPages: Math.ceil(total / paginationDto.limit),
+    };
+  }
+
+  async updateBank(
+    id: string,
+    updateData: Partial<CreateBankDto>,
+  ): Promise<BankResponseDto> {
+    const updatedBank = await this.bankRepository.update(id, updateData);
+    if (!updatedBank) {
+      throw new NotFoundException(`Bank with ID ${id} does not in database`);
+    }
+    return this.mapToResponseDto(updatedBank);
+  }
+
+  async deleteBank(id: string): Promise<MessageResponse> {
+    const deletedBank = await this.bankRepository.delete(id);
+    if (!deletedBank) {
+      return {
+        code: ERROR_RES.NOT_FOUND_ERROR.statusCode,
+        info: 'FAIL',
+        message: `Bank with ID ${id} not found`,
+      };
+    }
+    return {
+      code: ERROR_RES.SUCCESS.statusCode,
+      info: 'SUCCESS',
+      message: `Bank ${deletedBank.name} deleted successfully`,
+    };
   }
 }
