f2add7fc8dd8ecd6874e07f5c18723f8e271165e
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-05 11:45:12 +0700

    Add response DTOs for entity endpoints

---
+++ src/sale-transaction/dto/bank.res.ts
@@ -0,0 +1,6 @@
+import { MessageResponse } from '@app-types/message.res';
+import { Bank } from '../schemas/bank.schema';
+
+export class BankResponseDto extends MessageResponse {
+  content: Bank;
+}
---
+++ src/sale-transaction/dto/department.res.ts
@@ -0,0 +1,6 @@
+import { MessageResponse } from '@app-types/message.res';
+import { Department } from '../schemas/department.schema';
+
+export class DepartmentResponseDto extends MessageResponse {
+  content: Department;
+}
