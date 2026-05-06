154ae288c10fbbf44ec258a155fc47cc8e2ca610
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-06 08:52:56 +0700

    Adds department controller endpoints

--- src/department/department.controller.ts
+++ src/department/department.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+  ValidationPipe,
+} from '@nestjs/common';
+import { DepartmentService } from './department.service';
+import { CreateDepartmentDto } from './dto/create-department.req';
+import { DepartmentResponseDto } from './dto/department.res';
+import { ApiOperation } from '@nestjs/swagger';
+import {
+  PaginatedResponseDto,
+  PaginationDto,
+} from '@common/dto/pagination.dto';
+import { MessageResponse } from '@app-types/message.res';
 
 @Controller('departments')
-export class DepartmentController {}
+export class DepartmentController {
+  constructor(private readonly departmentService: DepartmentService) {}
+
+  @Post('create')
+  @ApiOperation({ summary: 'Create a new bank' })
+  @HttpCode(HttpStatus.CREATED)
+  async create(
+    @Body(ValidationPipe) createDepartmentDto: CreateDepartmentDto,
+  ): Promise<DepartmentResponseDto> {
+    return await this.departmentService.createDepartment(createDepartmentDto);
+  }
+
+  @Get()
+  @ApiOperation({ summary: 'Get a paginated list of banks' })
+  async findAll(
+    @Query(ValidationPipe) paginationDto: PaginationDto,
+  ): Promise<PaginatedResponseDto<DepartmentResponseDto>> {
+    return await this.departmentService.getAllDepartments(paginationDto);
+  }
+
+  @Get(':id')
+  @ApiOperation({ summary: 'Get department by ID' })
+  async findOne(@Param('id') id: string): Promise<DepartmentResponseDto> {
+    return await this.departmentService.getDepartmentById(id);
+  }
+
+  @Patch(':id')
+  @ApiOperation({ summary: 'Updated bank by ID' })
+  async update(
+    @Param('id') id: string,
+    @Body(ValidationPipe) updateBankDto: Partial<CreateDepartmentDto>,
+  ): Promise<DepartmentResponseDto> {
+    return await this.departmentService.updateDepartment(id, updateBankDto);
+  }
+
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete bank by ID' })
+  @HttpCode(HttpStatus.OK)
+  async remove(@Param('id') id: string): Promise<MessageResponse> {
+    return await this.departmentService.deleteDepartment(id);
+  }
+}
