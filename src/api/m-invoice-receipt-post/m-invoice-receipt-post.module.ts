2b677452111e2402fccfa92ca2b337249ee275d5
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-08 14:53:12 +0700

    update intergrated api module

--- src/api/m-invoice-receipt-post/m-invoice-receipt-post.module.ts
+++ src/api/m-invoice-receipt-post/m-invoice-receipt-post.module.ts
@@ -2,6 +2,12 @@ import { Module } from '@nestjs/common';
 import { MInvoiceReceiptPostService } from './m-invoice-receipt-post.service';
 import { MInvoiceReceiptPostController } from './m-invoice-receipt-post.controller';
 import { HttpModule } from '@nestjs/axios';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  SalesTransaction,
+  SalesTransactionSchema,
+} from '@schemas/sale-transaction.schema';
+import { SaleTransactionModule } from '@module/sale-transaction/sale-transaction.module';
 
 @Module({
   imports: [
@@ -9,6 +15,13 @@ import { HttpModule } from '@nestjs/axios';
       timeout: 5000,
       maxRedirects: 5,
     }),
+    MongooseModule.forFeature([
+      {
+        name: SalesTransaction.name,
+        schema: SalesTransactionSchema,
+      },
+    ]),
+    SaleTransactionModule,
   ],
   providers: [MInvoiceReceiptPostService],
   controllers: [MInvoiceReceiptPostController],
