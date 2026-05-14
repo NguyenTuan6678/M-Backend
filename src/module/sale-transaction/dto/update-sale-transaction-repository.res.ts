87b87412276bed6f0b97ded5bb5513b8aaf407ff
NguyenTuan6678 <canhdongtuyet39@gmail.com>
2026-05-14 10:40:20 +0700

    new dto for update

---
+++ src/module/sale-transaction/dto/update-sale-transaction-repository.res.ts
@@ -0,0 +1,8 @@
+export class UpdateSalesTransactionDto {
+  inv_invoiceSeries?: string;
+  key_api?: string;
+  inv_invoiceIssuedDate?: string;
+  inv_invoiceCreatedId?: string;
+  so_benh_an?: string;
+  activationDate?: string; // ← có ở đây
+}
